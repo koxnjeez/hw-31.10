6279266d3d0951e4ca89b4be099aa38cf6f6b0eb
koxnjeez <145009796+koxnjeez@users.noreply.github.com>
2023-11-01 23:19:48 +0200

    Add files via upload

---
+++ hw 31.10/hw 31.10/hw 31.10.cpp
@@ -0,0 +1,13 @@
+﻿#include "settings.h"
+
+int main()
+{
+    int a = 10;
+    int b = 28;
+    int* p_a = &a;
+    int* p_b = &b;
+    cout << p_a << "   " << p_b << "\n\n";
+    int distance = p_b - p_a;
+    cout << "Distance between a and b = " << distance << "B   OR   " << distance / 4 << " int type variable\n";
+    Sleep(INFINITE);
+}
